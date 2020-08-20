8e1c65a5b7fb0510dd66d1889cc6aea7173e2857
wzq <wzq@localhost.localdomain>
2020-08-20 18:13:31 +0800

    update

--- python_req.sh
+++ python_req.sh
@@ -1,5 +1,25 @@
+# for lorax
+
+sudo dnf install -y elfutils-libelf-devel
+sudo dnf install -y python3-devel python3-gobject gtk3 libgit2-glib libgit2
 sudo  pip3 install pytest-cov rpmfluff   ansible-runner pycdlib semantic_version
 
-sudo dnf install -y python3-devel python3-gobject gtk3 libgit2-glib libgit2
+
+# fro anaconda
+sudo dnf install lorax-lmc-virt libguestfs-tools python3-libvirt virt-install  \
+ 	glib2 glib2-devel
+
+#  install "parallel" from source code 
+
+
+
+#centos7 glade3-libgladeui glade3-libgladeui-devel
+
+
+dnf install   audit-libs-devel rpm-devel
+
+dnf --enablerepo=PowerTools install glade-devel libxklavier-devel gobject-introspection-devel \
+	libarchive-devel libarchive-devel gtk-doc
+
 
  
